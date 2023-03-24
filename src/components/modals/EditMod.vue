03e5f399e199081d68f1ac89ee6341e7b1f1e7af
knifoon <knifoon@gmail.com>
2023-03-24 02:44:11 -0700

    You can now change the character name, in mod edit

--- src/components/modals/EditMod.vue
+++ src/components/modals/EditMod.vue
@@ -5,6 +5,7 @@ const instance = getCurrentInstance();
 const {readFileSync, readdirSync, writeFileSync} = require('fs')
 const props = defineProps(['json'])
 
+let characterForm = ref(false)
 let modInfo = JSON.parse(props.json)
 let gimmFile = readdirSync(modInfo.path).find(file => file.startsWith('GIMM.json'))
 let gimm
@@ -22,7 +23,6 @@ if(gimmFile){
 const save = () => {
     console.log('from edit screen');
     let getOriginalName = modInfo.path.split('/')
-    // getOriginalName = getOriginalName
     let originalName = getOriginalName[getOriginalName.length - 1]
     if(gimm.value.name == "") gimm.value.name = originalName
     writeFileSync(`${modInfo.path}/GIMM.jsonc`, JSON.stringify(gimm.value))
@@ -33,12 +33,16 @@ const save = () => {
 <template>
     <div class="container">
         <div class="meta">
-            <h1>Editing Mod for {{ gimm.character || modInfo.character }}</h1>
+            <div class="characterTitle">
+                <h1>Editing Mod for {{ gimm.character || modInfo.character }}</h1>
+                <button @click="characterForm = true" title="Edit Character Name, this will move this mod under a different mod group">?</button>
+            </div>
             <div> Location: <span class="path">{{ modInfo.path.replaceAll('/','\\') }}</span></div>
             <div v-if="modInfo.readme"> ReadMe: <span class="path">{{ modInfo.readme }}</span></div>
             <div v-else>No ReadMe File Found</div>
         </div>
         <br>
+        <Form type="text" label="character" v-model:data="gimm.character" v-if="characterForm"/>
         <Form type="text" label="name" v-model:data="gimm.name"/>
         <Form type="text" label="version" v-model:data="gimm.version"/>
         <Form type="text" label="collection" v-model:data="gimm.collection"/>
@@ -47,6 +51,17 @@ const save = () => {
     </div>
 </template>
 <style scoped>
+.characterTitle {
+    display: flex;
+    align-items:center
+}
+.characterTitle h1 {
+    display: inline-block;
+}
+.characterTitle button{
+    display: inline-block;
+    margin-left: 10px;
+}
 .container {
     margin: auto;
 }
